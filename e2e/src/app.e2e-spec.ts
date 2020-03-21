5c65ca001d513568968357c1c2e4e9c1015ab1c9
Kosiarzenerek <dbabka.poczta@gmail.com>
2020-03-21 19:40:42 +0100

    Project rename and screenshots added

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('koffeecup-interview-task app is running!');
+    expect(page.getTitleText()).toEqual('images-loader app is running!');
   });
 
   afterEach(async () => {
